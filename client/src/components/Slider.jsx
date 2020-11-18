66a2e605a5855c77043f3757fa12c76898adf05a
lucyyxc <xilucychen@gmail.com>
2020-11-18 10:03:35 -0800

    changed css modules setup

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import css from './Slider.scss';
+import styles from './Slider.scss';
 import Description from './Description.jsx';
 
 function Slider({ listings }) {
   return (
-    <div className="slider">
+    <div className={styles.slider}>
       {listings.map((listing, index) => (
-        <div key={index} className="slide">
-          <div className="image">
+        <div key={index} className={styles.slide}>
+          <div className={styles.image}>
             <img src={listing.image} />
           </div>
           <Description id={listing.id} listing={listing} />
         </div>
       ))}
-      <button className="goLeft"><img src="https://s3-us-west-1.amazonaws.com/fec.similarhomes/FEC+avatars/larrow.svg" /></button>
-      <button className="goRight"><img src="https://s3-us-west-1.amazonaws.com/fec.similarhomes/FEC+avatars/rarrow.svg" /></button>
+      <button className={styles.goLeft}><img src="https://s3-us-west-1.amazonaws.com/fec.similarhomes/FEC+avatars/larrow.svg" /></button>
+      <button className={styles.goRight}><img src="https://s3-us-west-1.amazonaws.com/fec.similarhomes/FEC+avatars/rarrow.svg" /></button>
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,16 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
+        use: ['style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+              },
+            },
+          }],
       },
       {
         test: /\.scss$/i,
