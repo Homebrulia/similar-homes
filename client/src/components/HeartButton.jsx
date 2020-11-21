5ebac6100a7de33fb45f1f10f04f670208b923b6
lucyyxc <xilucychen@gmail.com>
2020-11-20 18:04:33 -0800

    fixed gitignore

---
+++ client/src/components/HeartButton.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react/button-has-type */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import React from 'react';
+import styles from './HeartButton.css';
+
+const HeartButton = ({
+  listing, heartId, saveListing, savedListing, changeImage,
+}) => (
+
+  <button
+    // type='submit'
+    id={styles.heartbtn}
+    onClick={() => saveListing()}
+  >
+
+    <img src="https://s3-us-west-1.amazonaws.com/fec.similarhomes/FEC+avatars/heartunclicked.svg" />
+  </button>
+
+);
+
+export default HeartButton;
+
+// savedListing.includes(id) ? styles.clickedheartbtn :
+
+{ /* <img src={savedListing.includes(id) ? "https://s3-us-west-1.amazonaws.com/fec.similarhomes/FEC+avatars/heartclicked.svg" : "https://s3-us-west-1.amazonaws.com/fec.similarhomes/FEC+avatars/heartunclicked.svg"}/> */ }
+
+{ /* {savedListing.includes(id) ? */ }
+{ /* // <img src="https://s3-us-west-1.amazonaws.com/fec.similarhomes/FEC+avatars/heartclicked.svg" /> : <img src="https://s3-us-west-1.amazonaws.com/fec.similarhomes/FEC+avatars/heartunclicked.svg" />} */ }
