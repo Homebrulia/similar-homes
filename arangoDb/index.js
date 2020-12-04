959dbb8db98b7327962b9c370645d7b03253adbd
Shailee Patel <shaileeee@gmail.com>
2020-12-04 15:26:31 -0800

    update arango schema

--- arangoDb/index.js
+++ arangoDb/index.js
@@ -20,6 +20,7 @@ db.createDatabase('similar_homes')
           bath: { type: "number"},
           sqft: { type: "number"},
           address: { type: "string", "maxLength" : 100},
+          similar_listings: { type: "array", items: { type: "object", maximum: 18 } }
         },
         required: ["url", "price", "bed", "bath", "sqft", "address"]
       },
@@ -30,28 +31,13 @@ db.createDatabase('similar_homes')
   })
   .then(() => {
     console.log('listings created');
-    var similarListings = db.collection('similar_listings');
-    var schema = {
-      rule: {
-        properties: {
-          listing_id: { type: "string"},
-          similar_listing_id: { type: "string"},
-        },
-        required: ["listing_id", "similar_listing_id"]
-      },
-      level: "moderate",
-      message: "Schema validation failed."
-    };
-    return similarListings.create({'schema': schema});
-  })
-  .then(() => {
-    console.log('similar_listings created');
     var users = db.collection('users');
     var schema = {
       rule: {
         properties: {
           _key: { type: "number"},
           name: { type: "string", "maxLength" : 20},
+          fav_listings: { type: "array", items: { type: "object"} }
         },
         required: ["name"]
       },
@@ -62,22 +48,6 @@ db.createDatabase('similar_homes')
   })
   .then(() => {
     console.log('users created');
-    var userFav = db.collection('user_fav');
-    var schema = {
-      rule: {
-        properties: {
-          listing_id: { type: "string"},
-          user_id: { type: "string"},
-        },
-        required: ["id", "name"]
-      },
-      level: "moderate",
-      message: "Schema validation failed."
-    };
-    return userFav.create({'schema': schema});
-  })
-  .then(() => {
-    console.log('user_fav created');
   })
   .catch(err => console.error('Failed to create:', err));
 
