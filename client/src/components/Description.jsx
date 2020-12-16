e65d299e60f2bd035f89052e340f664d4c2eca24
Shailee Patel <shaileeee@gmail.com>
2020-12-15 17:02:09 -0800

    front end works with postgres

--- client/src/components/Description.jsx
+++ client/src/components/Description.jsx
@@ -7,7 +7,7 @@ const Description = ({ listing, id }) => (
 
     <li> <img className={styles.avatar} src="https://s3-us-west-1.amazonaws.com/fec.similarhomes/FEC+avatars/bd.svg" /> {listing.bed}bd <img className={styles.avatar} src="https://s3-us-west-1.amazonaws.com/fec.similarhomes/FEC+avatars/ba.svg" /> {listing.bath}ba <img className={styles.avatar} src="https://s3-us-west-1.amazonaws.com/fec.similarhomes/FEC+avatars/sqft.svg" /> {listing.sqft}sqft </li>
 
-    <li> {listing.address} </li>
+    <li className={styles.address}> {listing.address} </li>
 
     <li className={styles.neighborhood}> {listing.neighborhood} </li>
   </ul>
