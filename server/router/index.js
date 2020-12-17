fe0fb300163ae917abc4bbd77ea1f3058a52f223
Shailee Patel <shaileeee@gmail.com>
2020-12-16 22:20:25 -0800

    added bash file for 2nd service instance

--- server/router/index.js
+++ server/router/index.js
@@ -8,4 +8,6 @@ router.use('/listings/:id', express.static(publicDir));
 
 router.get('/listings/:id/similar', controller.getSimilar);
 
+router.get('/loaderio-25354d69204a7fec2f13f8bfce9299df', (req, res) => {res.send('loaderio-25354d69204a7fec2f13f8bfce9299df')});
+
 module.exports = router;
